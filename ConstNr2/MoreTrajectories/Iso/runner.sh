ecba8ca42db141f68110e37239c18ada38abb8e3
sparedOstrich <shreeganeshasharma@gmail.com>
2024-02-04 20:19:18 +0530

    Simulations for more settings of pore size and number

---
+++ ConstNr2/MoreTrajectories/Iso/runner.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#Aim : Run Iso files with pores
+#Finds MFPT(s) for constant porous fraction
+#Convention : Isothermal
+#D1/D2 = 1/1, nump = 10,000, R1 = 0.5, R2 = 1, R = 0.3, r0 = 0.4
+#Author: Shree Ganesha Sharma M S
+#Date: January 23rd 2023
+
+declare -a filenames=(
+"3dWithPores_Iso_10_101.py"
+"3dWithPores_Iso_10_110.py"
+"3dWithPores_Iso_10_11.py"
+"3dWithPores_Iso_20_101.py"
+"3dWithPores_Iso_20_110.py"
+"3dWithPores_Iso_20_11.py"
+"3dWithPores_Iso_30_101.py"
+"3dWithPores_Iso_30_110.py"
+"3dWithPores_Iso_30_11.py"
+"3dWithPores_Iso_40_101.py"
+"3dWithPores_Iso_40_110.py"
+"3dWithPores_Iso_40_11.py"
+"3dWithPores_Iso_50_101.py"
+"3dWithPores_Iso_50_110.py"
+"3dWithPores_Iso_50_11.py"
+"3dWithPores_Iso_60_101.py"
+"3dWithPores_Iso_60_110.py"
+"3dWithPores_Iso_60_11.py"
+"3dWithPores_Iso_70_101.py"
+"3dWithPores_Iso_70_110.py"
+"3dWithPores_Iso_70_11.py"
+"3dWithPores_Iso_80_101.py"
+"3dWithPores_Iso_80_110.py"
+"3dWithPores_Iso_80_11.py"
+"3dWithPores_Iso_90_101.py"
+"3dWithPores_Iso_90_110.py"
+"3dWithPores_Iso_90_11.py"
+"3dWithPores_Iso_100_101.py"
+"3dWithPores_Iso_100_110.py"
+"3dWithPores_Iso_100_11.py")
+
+
+for value in "${!filenames[@]}"
+do
+	echo >> loggerIso.txt
+	echo ${filenames[value]}
+	python3 ${filenames[value]} >> loggerIso.txt
+ 	echo >> loggerIso.txt
+done
---
+++ ConstNr2/MoreTrajectories/Str/runner.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#Aim : Run Str files with pores
+#Finds MFPT(s) for constant porous fraction
+#Convention : Str
+#D1/D2 = 1/1, nump = 10,000, R1 = 0.5, R2 = 1, R = 0.3, r0 = 0.4
+#Author: Shree Ganesha Sharma M S
+#Date: January 23rd 2023
+
+declare -a filenames=(
+"3dWithPores_Str_10_101.py"
+"3dWithPores_Str_10_110.py"
+"3dWithPores_Str_10_11.py"
+"3dWithPores_Str_20_101.py"
+"3dWithPores_Str_20_110.py"
+"3dWithPores_Str_20_11.py"
+"3dWithPores_Str_30_101.py"
+"3dWithPores_Str_30_110.py"
+"3dWithPores_Str_30_11.py"
+"3dWithPores_Str_40_101.py"
+"3dWithPores_Str_40_110.py"
+"3dWithPores_Str_40_11.py"
+"3dWithPores_Str_50_101.py"
+"3dWithPores_Str_50_110.py"
+"3dWithPores_Str_50_11.py"
+"3dWithPores_Str_60_101.py"
+"3dWithPores_Str_60_110.py"
+"3dWithPores_Str_60_11.py"
+"3dWithPores_Str_70_101.py"
+"3dWithPores_Str_70_110.py"
+"3dWithPores_Str_70_11.py"
+"3dWithPores_Str_80_101.py"
+"3dWithPores_Str_80_110.py"
+"3dWithPores_Str_80_11.py"
+"3dWithPores_Str_90_101.py"
+"3dWithPores_Str_90_110.py"
+"3dWithPores_Str_90_11.py"
+"3dWithPores_Str_100_101.py"
+"3dWithPores_Str_100_110.py"
+"3dWithPores_Str_100_11.py")
+
+
+for value in "${!filenames[@]}"
+do
+	echo >> loggerStr.txt
+	echo ${filenames[value]}
+	python3 ${filenames[value]} >> loggerStr.txt
+ 	echo >> loggerStr.txt
+done
